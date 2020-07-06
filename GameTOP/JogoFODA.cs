6032ebe7441efe7797c5e6ba2a53e1d6f2adbdca
DanielODev <danielorlandi.dev@gmail.com>
2020-07-06 14:35:13 -0300

    first commit

---
+++ GameTOP/JogoFODA.cs
@@ -0,0 +1,29 @@
+using System;
+using GameTOP.Lib;
+
+namespace GameTOP
+
+{
+    public class JogoFODA
+    {
+            private readonly iJogador _jogadorA;
+            private readonly iJogador _jogadorB;
+            public JogoFODA(iJogador jogadorA, iJogador jogadorB)
+            {
+                _jogadorA = jogadorA;
+                _jogadorB = jogadorB;
+            }
+            public void IniciarJogo()
+            {
+                System.Console.Write(_jogadorA.Corre());
+                System.Console.Write(_jogadorA.Chuta());
+                System.Console.Write(_jogadorA.Passe());
+
+                System.Console.Write("\n Próximo Jogador \n\n");
+
+                System.Console.Write(_jogadorB.Corre());
+                System.Console.Write(_jogadorB.Chuta());
+                System.Console.Write(_jogadorB.Passe());
+            }
+    }
+}
